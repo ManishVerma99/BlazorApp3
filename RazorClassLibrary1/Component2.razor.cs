504dad64cb4ba8dffa920902d5811dbc83aa792d
ManishVerma99 <manishpar62806@gmail.com>
2022-10-15 12:07:15 +0530

    Add project files.

---
+++ RazorClassLibrary1/Component2.razor.cs
@@ -0,0 +1,13 @@
+﻿using Syncfusion.Blazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary1
+{
+    public class Component2Base: BaseComponent
+    {
+    }
+}
